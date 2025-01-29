5303d11a231ca65492a02b760603499687ecbe6a
Henrique Mateus de Oliveira Pereira <75270579+Henriquejogos@users.noreply.github.com>
2025-01-28 23:15:51 -0300

    Add files via upload

---
+++ include/Jogo.hpp
@@ -0,0 +1,17 @@
+#ifndef JOGO_HPP
+#define JOGO_HPP
+
+class Jogo {
+public:
+    virtual ~Jogo() = default;
+    virtual int getAltura() const = 0;
+    virtual int getLargura() const = 0;
+    virtual void iniciar() = 0;
+    virtual void imprimirTabuleiro() = 0;
+    virtual bool validarJogada(int linha, int coluna) = 0;
+    virtual bool verificarVitoria() = 0;
+    virtual void realizarJogada(int linha, int coluna) = 0;
+    virtual bool empatePartida() = 0;
+};
+
+#endif
